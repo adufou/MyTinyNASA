3797b7e9153b0a2c5534119b1a367c713f7065ab
Leo Tavernier <leo.tavernier@epita.fr>
2021-06-14 17:50:46 +0200

    Created the apod section

---
+++ app/src/main/java/com/example/mytinynasa/ui/apod/ApodFragment.kt
@@ -0,0 +1,32 @@
+package com.example.mytinynasa.ui.apod
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.mytinynasa.R
+
+class ApodFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ApodFragment()
+    }
+
+    private lateinit var viewModel: ApodViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.apod_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(ApodViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/mytinynasa/ui/apod/ApodViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.mytinynasa.ui.apod
+
+import androidx.lifecycle.ViewModel
+
+class ApodViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
