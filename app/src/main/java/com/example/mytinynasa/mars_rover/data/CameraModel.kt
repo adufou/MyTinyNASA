a7102512e0b847654be06e7e0298e8fc035d7bd6
Louis SANTUCCI <louis.santucci@epita.fr>
2021-06-16 22:47:44 +0200

    [MARSROVER] Updated data classes to read the API call

---
+++ app/src/main/java/com/example/mytinynasa/mars_rover/data/CameraModel.kt
@@ -0,0 +1,3 @@
+package com.example.mytinynasa.mars_rover.data
+
+data class CameraModel (val name : String)
---
+++ app/src/main/java/com/example/mytinynasa/mars_rover/data/RoverModel.kt
@@ -0,0 +1,3 @@
+package com.example.mytinynasa.mars_rover.data
+
+data class RoverModel (val name : String)
