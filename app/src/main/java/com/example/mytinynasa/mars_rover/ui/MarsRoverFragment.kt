287b1f6e840f8f25fd94fce23a0e71dc078ed21d
Louis SANTUCCI <louis.santucci@epita.fr>
2021-06-16 20:40:18 +0200

    [MARSROVER] Added Data class for list of results of photos

--- app/src/main/java/com/example/mytinynasa/mars_rover/ui/MarsRoverFragment.kt
+++ app/src/main/java/com/example/mytinynasa/mars_rover/ui/MarsRoverFragment.kt
@@ -1,33 +1,60 @@
 package com.example.mytinynasa.mars_rover.ui
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.mytinynasa.R
-import com.example.mytinynasa.mars_rover.MarsRoverActivity
 
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [MarsRoverFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
 class MarsRoverFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
 
-    companion object {
-        fun newInstance() = MarsRoverFragment()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
     }
-
-    private lateinit var activity: MarsRoverActivity
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.mars_rover_item_list, container, false)
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_mars_rover, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        activity = ViewModelProvider(this).get(MarsRoverActivity::class.java)
-        // TODO: Use the ViewModel
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment MarsRoverFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            MarsRoverFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
     }
-
 }
