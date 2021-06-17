9dfe772ef32d7101d7c018e809dc61cb58c77c70
Leo Tavernier <leo.tavernier@epita.fr>
2021-06-17 19:30:49 +0200

    [APOD] Pictures are loaded, author and title too but the explanation is croped so need to fix that

--- app/src/main/java/com/example/mytinynasa/apod/data/ApodModel.kt
+++ app/src/main/java/com/example/mytinynasa/apod/data/ApodModel.kt
@@ -1,4 +1,4 @@
 package com.example.mytinynasa.apod.data
 
-data class ApodModel (val img_src: String, val title: String, val explanation: String, val  author: String) {
+data class ApodModel (val url: String, val title: String, val explanation: String, val  copyright: String) {
 }
