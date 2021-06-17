653bbdb46747abbb595ac1d41a115e15391e8987
Leo Tavernier <leo.tavernier@epita.fr>
2021-06-17 10:28:37 +0200

    [APOD] Commit to merge and get the network part

--- app/src/main/java/com/example/mytinynasa/ui/apod/ApodFragment.kt
+++ app/src/main/java/com/example/mytinynasa/ui/apod/ApodFragment.kt
@@ -7,6 +7,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.mytinynasa.R
+import com.example.mytinynasa.network.ApiClient
+import com.example.mytinynasa.network.ApiInterface
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ApodFragment : Fragment() {
 
@@ -29,8 +34,45 @@ class ApodFragment : Fragment() {
         // TODO: Use the ViewModel
     }
 
-    private fun getAPODPhotos(apiKey: String) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val data = "";
+
+        val eonetRecyclerView: RecyclerView = requireView().findViewById(R.id.eonet_recycler)
+        eonetRecyclerView.layoutManager = LinearLayoutManager(context)
+        eonetRecyclerView.addItemDecoration(
+            DividerItemDecoration(
+                context,
+                LinearLayoutManager.VERTICAL
+            )
+        )
+
+        // RETROFIT
+        val retrofit = ApiClient.getApiClient("https://eonet.sci.gsfc.nasa.gov/api/v3/")
+        val service = retrofit.create(ApiInterface::class.java)
+
+        val callback: Callback<EonetResult> = object : Callback<EonetResult> {
+            override fun onResponse(call: Call<EonetResult>, response: Response<EonetResult>) {
+                if (response.isSuccessful) {
+                    response.body()?.let { body ->
+                        Log.d("MyTinyNasa", "EONET API Success : " + body.toString())
+
+                        data = body.events as ArrayList<Event>
+                        eonetRecyclerView.adapter = EonetAdapter(data)
+                    }
+                } else {
+                    Log.d("MyTinyNasa", "EONET API Error : " + response.errorBody().toString())
+                }
+            }
+
+            override fun onFailure(call: Call<EonetResult>, t: Throwable) {
+                Log.d("MyTinyNasa", "EONET API Error : " + t.message)
+            }
+
+        }
+        service.getEonetEvents(null, null, null, 25, null, null, null, null, null, null, null)
+            .enqueue(callback)
     }
 
 }
