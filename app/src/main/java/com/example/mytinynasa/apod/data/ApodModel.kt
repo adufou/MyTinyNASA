34e12247330ad8e6e443ded3e6e79be2eae8e547
Leo Tavernier <leo.tavernier@epita.fr>
2021-06-17 12:29:45 +0200

    [APOD] Finished the apod adapter and first implem of the apod_item_list

--- app/src/main/java/com/example/mytinynasa/apod/data/ApodModel.kt
+++ app/src/main/java/com/example/mytinynasa/apod/data/ApodModel.kt
@@ -1,3 +1,4 @@
 package com.example.mytinynasa.apod.data
 
-data class ApodModel (val name : String)
+data class ApodModel (val img_src: String, val title: String, val explanation: String, val  author: String) {
+}
--- app/src/main/java/com/example/mytinynasa/apod/ui/ApodAdapter.kt
+++ app/src/main/java/com/example/mytinynasa/apod/ui/ApodAdapter.kt
@@ -1,4 +1,45 @@
 package com.example.mytinynasa.apod.ui
 
-class ApodAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.mytinynasa.R
+import com.example.mytinynasa.apod.data.ApodModel
+
+class ApodAdapter(val data : List<ApodModel>) : RecyclerView.Adapter<ApodAdapter.ApodHolder>() {
+    class ApodHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(model : ApodModel) {
+            val imgSrcImageView = itemView.findViewById<ImageView>(R.id.apod_item_image)
+            val titleTextView : TextView = itemView.findViewById(R.id.apod_title)
+            val explanationTextView : TextView = itemView.findViewById(R.id.apod_explanation)
+            val authorTextView : TextView = itemView.findViewById(R.id.apod_author)
+            Glide.with(itemView.context).load(model.img_src).centerCrop().into(imgSrcImageView)
+            titleTextView.text = model.title
+            explanationTextView.text = model.explanation
+            authorTextView.text = model.author
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ApodHolder {
+
+        val viewHolder : ApodHolder
+        val rowView : View = LayoutInflater.from(parent.context).inflate(R.layout.apod_item_list, parent, false)
+        viewHolder = ApodHolder(rowView)
+
+        return viewHolder
+    }
+
+    override fun onBindViewHolder(holder: ApodHolder, position: Int) {
+        holder.bind(data[position])
+        holder.itemView.tag = position
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
 }
