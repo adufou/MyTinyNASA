83fb351eade1b91a3a444e9f96581623ff7b8dd2
Leo Tavernier <leo.tavernier@epita.fr>
2021-06-25 20:30:14 +0200

    [APOD] Fixed issue with videos, replaced videos by picture of the sky

--- app/src/main/java/com/example/mytinynasa/apod/data/ApodModel.kt
+++ app/src/main/java/com/example/mytinynasa/apod/data/ApodModel.kt
@@ -1,4 +1,4 @@
 package com.example.mytinynasa.apod.data
 
-data class ApodModel (val url: String, val title: String, val explanation: String, val  copyright: String) {
+data class ApodModel (val url: String, val title: String, val explanation: String, val  copyright: String, val media_type: String) {
 }
